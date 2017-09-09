431e61a1c52d8738ba46eba9c5c839028c7bb7b1
Suraj Kumar Jana <surajjana2@gmail.com>
2017-09-09 14:46:54 +0530

    parser.py updated @surajjana

--- py/parse.py
+++ py/parse.py
@@ -1,7 +1,28 @@
 
 fname = 'sample_feature.txt'
 
+keywords = ['Given', 'And', 'When', 'Then']
+
 with open(fname) as fd:
     data = fd.readlines()
 
-print data
+feature = [ data[0].split('Feature: ')[1].split('\n')[0], data[1].split('\n')[0]]
+
+scenario = data[2].split('Scenario: ')[1].split('\n')[0]
+
+res = {"feature": feature, "scenario": { "name": scenario, "cond": []}}
+
+for i in range(3, len(data)):
+	cond = {'key': '', 'value': ''}
+
+	d1 = data[i].split('\n')[0]
+	key = d1.split(' ',1)[0]
+
+	if(key in keywords):
+		
+		cond['key'] = key
+		cond['value'] = d1.split(' ',1)[1]
+
+		res['scenario']['cond'].append(cond)
+
+print res
